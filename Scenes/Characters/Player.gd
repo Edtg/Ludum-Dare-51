ad93ee6b266ca68f77dedfcac487754ebc4701b0
Edward Green <ed@teamgreen.co.uk>
2022-10-02 15:11:32 +0100

    Updated Player Sprite and added floor tiles

--- Scenes/Characters/Player.gd
+++ Scenes/Characters/Player.gd
@@ -12,13 +12,26 @@ func _physics_process(delta):
 	
 	if Input.is_action_pressed("MoveUp"):
 		velocity.y -= 1
+		$Sprite.flip_v = false
+		$Sprite.rotation_degrees = 0
 	if Input.is_action_pressed("MoveDown"):
 		velocity.y += 1
+		$Sprite.flip_v = true
+		$Sprite.rotation_degrees = 0
 	if Input.is_action_pressed("MoveLeft"):
 		velocity.x -= 1
+		$Sprite.flip_v = true
+		$Sprite.rotation_degrees = 90
 	if Input.is_action_pressed("MoveRight"):
 		velocity.x += 1
+		$Sprite.flip_v = false
+		$Sprite.rotation_degrees = 90
 	
 	if canMove:
+		if velocity != Vector2(0,0):
+			$Sprite.playing = true
+		else:
+			$Sprite.playing = false
+			$Sprite.frame = 0
 		move_and_collide(velocity * speed)
 
