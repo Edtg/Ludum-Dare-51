123e85298cc31ab7089e3cde1f689163176d5abd
Edward Green <ed@teamgreen.co.uk>
2022-10-03 15:17:50 +0100

    Finished dialogue functionality

--- Scenes/UI/Dialogue.gd
+++ Scenes/UI/Dialogue.gd
@@ -6,14 +6,22 @@ var currentLine = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	get_tree().paused = true
+	visible = true
+	PlayNextDialogue()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	if Input.is_action_just_pressed("DialogueContinue"):
+		if currentLine < len(dialogueLines):
+			PlayNextDialogue()
+		else:
+			get_tree().paused = false
+			visible = false
 
 func PlayNextDialogue():
 	var dialogue = dialogueLines[currentLine]
+	$WalkieTalkie/SpeechBubble/SpeechLabel.text = dialogue
 	currentLine += 1
 	$AnimationPlayer.play("Broadcast")
